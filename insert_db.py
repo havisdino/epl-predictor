0c4366cc446fafd861ac086f13d46bc7d4c5e651
havisdino <hadinh893@gmail.com>
2025-05-28 07:18:01 +0700

    kkk

--- insert_db.py
+++ insert_db.py
@@ -1,7 +1,8 @@
 import sys
-from utils.sqlite_tools import update_sqlite_with_new_records
+from utils.sqlite_tools import recompute_team_stats, update_sqlite_with_new_records
 
 
 for jsonl_file in sys.argv[1:]:
     print(f"Updating SQLite database with records from {jsonl_file}")
     update_sqlite_with_new_records(jsonl_file, db_path="data/epl.db")
+    recompute_team_stats(db_path="data/epl.db")
